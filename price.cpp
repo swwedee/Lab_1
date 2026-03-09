3198bf0590e0acf764baabcb985fa2125ba7bc35
Sofya Shveda <SOFIIA.SHVEDA@lnu.edu.ua>
2026-03-09 16:56:21 +0200

    price.cpp

--- price.cpp
+++ price.cpp
@@ -64,15 +64,11 @@ void runProgram()
 
     while (file >> name)
     {
-       file >> name;
-       file >> name;
 
        file >> grn;
-       file >> name;
        file >> kop;
-       file >> name;
        file >> count;
-       file >> name;
+        
 
         Price one = {grn, kop};
         Price multiplied = {0, 0};
@@ -89,6 +85,6 @@ void runProgram()
 
     roundPrice(total);
 
-    cout << "Сума до оплати (після заокруглення): ";
+    cout << "Сума до оплати після заокруглення: ";
     printPrice(total);
 }
